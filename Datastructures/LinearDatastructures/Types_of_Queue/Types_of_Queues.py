eb6a6d346e46b945de4257459ea26f8618045143
adaveniprashanth <adaveniprashanth@gmail.com>
2022-10-20 15:55:01 +0530

    Update Types_of_Queues.py
    new changes

--- Datastructures/LinearDatastructures/Types_of_Queue/Types_of_Queues.py
+++ Datastructures/LinearDatastructures/Types_of_Queue/Types_of_Queues.py
@@ -10,3 +10,42 @@ Double Ended Queue
 1.Simple Queue
 In a simple queue, insertion takes place at the rear and removal occurs at the front. It strictly follows the FIFO (First in First out) rule.
 '''
+
+
+
+class Queue:
+
+    def __init__(self):
+        self.queue = []
+
+    # Add an element
+    def enqueue(self, item):
+        self.queue.append(item)
+
+    # Remove an element
+    def dequeue(self):
+        if len(self.queue) < 1:
+            return None
+        return self.queue.pop(0)
+
+    # Display  the queue
+    def display(self):
+        print(self.queue)
+
+    def size(self):
+        return len(self.queue)
+
+
+q = Queue()
+q.enqueue(1)
+q.enqueue(2)
+q.enqueue(3)
+q.enqueue(4)
+q.enqueue(5)
+
+q.display()
+
+q.dequeue()
+
+print("After removing an element")
+q.display()
