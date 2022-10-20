3ab4338244b1ac141e3474ba258f3eae77a2642c
adaveniprashanth <adaveniprashanth@gmail.com>
2022-10-20 15:48:12 +0530

    Create Types_of_Queue.py
    Types of queues

---
+++ Datastructures/LinearDatastructures/Types_of_Queue.py
@@ -0,0 +1,12 @@
+'''
+There are four different types of queues:
+
+Simple Queue
+Circular Queue
+Priority Queue
+Double Ended Queue
+'''
+'''
+1.Simple Queue
+In a simple queue, insertion takes place at the rear and removal occurs at the front. It strictly follows the FIFO (First in First out) rule.
+'''
