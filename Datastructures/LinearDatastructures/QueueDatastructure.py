e1b4b6040e419337b5fe7976c3e866328fe0e161
adaveniprashanth <adaveniprashanth@gmail.com>
2022-10-20 15:38:36 +0530

    Create QueueDatastructure.py
    addition of Queue structure

---
+++ Datastructures/LinearDatastructures/QueueDatastructure.py
@@ -0,0 +1,36 @@
+'''
+3.Queue Data structure:
+    Unlike stack, the queue data structure works in the FIFO principle 
+    where first element stored in the queue will be removed first.
+'''
+if 1:
+    # Queue implementation in Python
+    class Queue:
+        def __init__(self):
+            self.queue = []
+
+        # Add an element
+        def enqueue(self, item):
+            self.queue.append(item)
+        # Remove an element
+        def dequeue(self):
+            if len(self.queue) < 1:
+                return None
+            return self.queue.pop(0)
+
+        # Display  the queue
+        def display(self):
+            print(self.queue)
+        def size(self):
+            return len(self.queue)
+
+    q = Queue()
+    q.enqueue(1)
+    q.enqueue(2)
+    q.enqueue(3)
+
+    q.display()
+    q.dequeue()
+
+    print("After removing an element")
+    q.display()
