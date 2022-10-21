2a18695a3a31218981b52cde8e2ae22b22edd9df
adaveniprashanth <adaveniprashanth@gmail.com>
2022-10-21 19:20:21 +0530

    Create Tree_traversal.py
    traversal of tree.

---
+++ Datastructures/NonLinearDatastructures/Tree_traversal.py
@@ -0,0 +1,13 @@
+# Traversing a tree means visiting every node in the tree.
+
+'''
+Linear data structures like arrays, stacks, queues, and linked list have only one way to read the data. 
+But a hierarchical data structure like a tree can be traversed in different ways.
+'''
+
+'''
+Types of traversal:
+1.Inorder: (left -> root -> right)
+2.Preorder: (root -> left -> right)
+3.postorder: (left -> right -> root)
+'''
