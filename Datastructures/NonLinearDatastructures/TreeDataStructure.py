7d9466280f6061656448d04b207dd2daea5d29f0
adaveniprashanth <adaveniprashanth@gmail.com>
2022-10-21 19:13:41 +0530

    Create TreeDataStructure.py
    Tree data strcutures file addition

---
+++ Datastructures/NonLinearDatastructures/TreeDataStructure.py
@@ -0,0 +1,48 @@
+'''
+A tree is a nonlinear hierarchical data structure that consists of nodes connected by edges.
+
+Why Tree Data Structure?
+  Other data structures such as arrays, linked list, stack, and queue are linear data structures that store data sequentially. 
+In order to perform any operation in a linear data structure, the time complexity increases with the increase in the data size.
+'''
+
+'''
+Tree Terminologies:
+1.Node:
+  A node is an entity that contains a key or value and pointers to its child nodes.
+
+2.Edge:
+  It is the link between any two nodes.
+
+3.Root:
+  It is the topmost node of a tree.
+  
+4.Height of a Node:
+  The height of a node is the number of edges from the node to the deepest leaf (ie. the longest path from the node to a leaf node).
+
+5.Depth of a Node:
+  The depth of a node is the number of edges from the root to the node.
+
+6.Height of a Tree:
+  The height of a Tree is the height of the root node or the depth of the deepest node.
+
+7.Degree of a Node:
+  The degree of a node is the total number of branches of that node.
+
+8.Forest:
+  A collection of disjoint trees is called a forest.
+'''
+
+'''
+Types of Tree:
+1.Binary Tree
+2.Binary Search Tree
+3.AVL Tree
+4.B-Tree
+'''
+
+'''
+Tree Traversal:
+  In order to perform any operation on a tree, you need to reach to the specific node.
+The tree traversal algorithm helps in visiting a required node in the tree.
+'''
