480f9d8a78fc9bc751ccf0089fa53f7c46e6740a
adaveniprashanth <adaveniprashanth@gmail.com>
2022-10-21 19:03:12 +0530

    Create NonLinearDatastructures.py
    Non linear data structures

---
+++ Datastructures/NonLinearDatastructures.py
@@ -0,0 +1,32 @@
+'''
+Non linear data structures:
+  Unlike linear data structures, elements in non-linear data structures are not in any sequence. 
+Instead they are arranged in a hierarchical manner where one element will be connected to one or more elements.
+'''
+
+# Non-linear data structures are further divided into graph and tree based data structures.
+
+'''
+1. Graph Data Structure:
+  In graph data structure, each node is called vertex and each vertex is connected to other vertices through edges.
+Popular Graph Based Data Structures:
+
+a.Spanning Tree and Minimum Spanning Tree
+b.Strongly Connected Components
+c.Adjacency Matrix
+d.Adjacency List
+'''
+
+'''
+2. Trees Data Structure
+  Similar to a graph, a tree is also a collection of vertices and edges. However, in tree data structure, there can only be one edge between two vertices.
+Popular Tree based Data Structure
+
+a.Binary Tree
+b.Binary Search Tree
+c.AVL Tree
+d.B-Tree
+e.B+ Tree
+f.Red-Black Tree
+'''
+
