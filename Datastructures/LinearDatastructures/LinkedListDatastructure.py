fbe08a295c7bb82fcb034fb8251750caf7192e3c
adaveniprashanth <adaveniprashanth@gmail.com>
2022-10-21 08:02:57 +0530

    Create LinkedListDatastructure.py
    new file for linked list datastructure

---
+++ Datastructures/LinearDatastructures/LinkedListDatastructure.py
@@ -0,0 +1,6 @@
+'''
+4.Linked List Data structure:
+    In linked list data structure, data elements are connected through a series of nodes. 
+    And, each node contains the data items and address to the next node.
+    Linked lists can be of multiple types: singly, doubly, and circular linked list.
+'''
