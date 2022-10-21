297b695dd979abf29617a9ac8d245fc5a3444142
adaveniprashanth <adaveniprashanth@gmail.com>
2022-10-21 19:34:09 +0530

    Update Tree_traversal.py
    Traversal code

--- Datastructures/NonLinearDatastructures/Tree_traversal.py
+++ Datastructures/NonLinearDatastructures/Tree_traversal.py
@@ -11,3 +11,40 @@ Types of traversal:
 2.Preorder: (root -> left -> right)
 3.postorder: (left -> right -> root)
 '''
+
+# Tree traversal
+if 1:
+    class Node:
+        def __init__(self,data):
+            self.data = data
+            self.left = None
+            self.right = None
+
+    def inorder(root):
+        if root:
+            inorder(root.left)
+            print(root.data,end=" ")
+            inorder(root.right)
+
+    def preorder(root):
+        if root:
+            print(root.data,end=" ")
+            preorder(root.left)
+            preorder(root.right)
+
+    def postorder(root):
+        if root:
+            postorder(root.left)
+            postorder(root.right)
+            print(root.data,end=" ")
+
+    root = Node(1)
+    root.left = Node(2)
+    root.right = Node(3)
+    root.left.left = Node(4)
+    root.left.right = Node(5)
+    root.right.left = Node(6)
+    root.right.right = Node(7)
+    inorder(root)
+    preorder(root)
+    postorder(root)
