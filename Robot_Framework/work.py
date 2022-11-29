0368d21bacb237d5810083e16e50f0193dca6404
adaveniprashanth <adaveniprashanth@gmail.com>
2022-11-29 10:31:55 +0530

    Create work.py
    new file addition for robot framework

---
+++ Robot_Framework/work.py
@@ -0,0 +1,11 @@
+RobotFramework
+http://robotframework.org/robotframework/4.0/RobotFrameworkUserGuide.html
+https://robotframework.org/robotframework/latest/libraries/Dialogs.html
+https://robotframework.org/robotframework/latest/libraries/Process.html
+https://robotframework.org/robotframework/latest/libraries/Screenshot.html
+https://robotframework.org/robotframework/latest/libraries/DateTime.html
+Core Python
+https://www.programiz.com/python-programming/examples
+https://www.geeksforgeeks.org/python-programming-examples/
+
+https://www.w3schools.com/python/python_regex.asp
