278a8e757ba68b6c83baff693f5fbd2487221f9f
adaveniprashanth <adaveniprashanth@gmail.com>
2022-12-14 19:40:01 +0530

    Update hello.py
    new changes

--- hello.py
+++ hello.py
@@ -2,7 +2,7 @@ import os
 import subprocess
 print("new changes")
 print("hello world")
-print("new from local again")
+print("new from local again and again")
 # checking subprocess and os module for installing and running the commands and any other operations
 subprocess.run("pip install selenium")
 os.system("pip uninstall selenium")
