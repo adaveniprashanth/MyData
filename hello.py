135d19410ccac7c3ccb4e553d525dc5d153ce81f
Adaveni <prashanthx.adaveni@intel.com>
2022-12-14 17:43:56 +0530

    commit message from python script1

--- hello.py
+++ hello.py
@@ -3,6 +3,6 @@ import subprocess
 print("new changes")
 print("hello world")
 print("new from local again")
-# checking subprocess and os module for installing and running the commands
+# checking subprocess and os module for installing and running the commands and any other operations
 subprocess.run("pip install selenium")
 os.system("pip uninstall selenium")
