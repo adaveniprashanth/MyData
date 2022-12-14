d42631c113775d679818fc44a0a4b806c69e5c43
Adaveni <prashanthx.adaveni@intel.com>
2022-12-14 21:34:16 +0530

    added new changes for 2 files

--- hello.py
+++ hello.py
@@ -6,3 +6,4 @@ print("new from local again and again")
 # checking subprocess and os module for installing and running the commands and any other operations
 subprocess.run("pip install selenium")
 os.system("pip uninstall selenium")
+print("hello world")
