e9c7a8454c10565c71c0837ac3e1146058188f11
padavenx <padavenx@padavenx-mobl>
2022-12-14 17:46:17 +0550

    my commit description

--- hello.py
+++ hello.py
@@ -1,3 +1,8 @@
+import os
+
 print("new changes")
 print("hello world")
 print("new from local again")
+import subprocess
+# subprocess.run("pip install selenium")
+os.system("pip uninstall selenium")
