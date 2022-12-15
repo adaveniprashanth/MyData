39b4884931873c5db7acacef92b4ccb788aacc20
adaveniprashanth <adaveniprashanth@gmail.com>
2022-12-15 16:18:47 +0530

    Update hello.py
    new changes added

--- hello.py
+++ hello.py
@@ -9,3 +9,14 @@ os.system("pip uninstall selenium")
 print("hello world")
 
 
+project name--> AXG_MediaIP_Val_PSVCOE
+
+Employee Id	CSB03603
+Employee Name	Prashanth Adaveni
+Old Project	Cerium-Internal-Bench
+New Project	AXG_MediaIP_Val_PSVCOE
+Old Po Number	ceri-int-002
+New Po Number	ceri-intc-425
+Old Customer Po Number	ceri-int-002
+New Customer Po Number	3001834106
+Effective Date	12-12-2022
