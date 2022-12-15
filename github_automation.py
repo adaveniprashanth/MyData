e05367bbbbeb59caab97665559052d5e3d60b6f9
Adaveni <prashanthx.adaveni@intel.com>
2022-12-15 19:12:25 +0530

    commit message from python script1

--- github_automation.py
+++ github_automation.py
@@ -38,8 +38,6 @@ if 0:
         dst_path = os.path.join(local_github,'github_configurations.ini')
         shutil.copy(src_path, dst_path)
         print('Copied')
-# print(os.path.exists(os.path.join(local_github,'github_configurations.ini')))
-
 
 
 #importing values from the config file
@@ -49,20 +47,19 @@ try:
 except FileNotFoundError:
     sys.exit("file not present. please check file")
 
-initialization = int(config["GITHUBSettings"]["init"])
+init = int(config["GITHUBSettings"]["init"])
 clone = int(config["GITHUBSettings"]["clone"])
-pull = 0#int(config["GITHUBSettings"]["pull"])
-push = 0#int(config["GITHUBSettings"]["push"])
+pull = int(config["GITHUBSettings"]["pull"])
+push = int(config["GITHUBSettings"]["push"])
 
 
 
 # initializing the repository
-# making one folder in local machine as git hub repo
-if initialization:
+# making one folder in local machine as github repo
+if init:
     print("initialising the local folder as local git repository")
     repository = Repo.init(local_github)
     print("created local repository")
-
 
 
 if clone:
@@ -70,125 +67,103 @@ if clone:
     git.Git(local_github).clone("https://github.com/adaveniprashanth/MyData.git")
     print("cloning is completed")
 
-
-'''
+# creating the handle for repository
 repo=git.Repo(os.path.join(local_github,'MyData'))
-'''
 
-'''
 if pull:
     print("pull request running")
     repo.git.checkout('master')#checkout to the particular branch
     origin = repo.remote(name='origin')
     origin.pull()
     print("pull request completed")
-'''
 
-# repo.git.checkout('-b','new_branch')#to create a PR we can create new branch and push changes from the branch
-'''
+
+if 1:
+    # copying the current python file to local git repository.
+    print("copying the current python file to local git repository.")
+    src_path = os.path.join(os.getcwd(), 'github_automation.py')
+    dst_path = os.path.join(local_github,'MyData','github_automation.py')
+    shutil.copy(src_path, dst_path)
+    print('Copied')
+
+# repo.git.checkout('-b','new_branch')#to create a PR, we can create new branch
+# and push changes from the branch
+
 if push:
-    # repo.git.checkout('-b', 'new_branch')
     print("push request running")
     repo.git.add('--all')
     repo.git.commit('-m', 'commit message from python script1')
     origin = repo.remote('origin')
     origin.push()
     print("push request completed")
-'''
 
-'''
-# we can use the below logic also to push the code
-# repo.git.add('--all')
-# repo.index.add(['bla.txt'])#for sing file addition
-# repo.index.commit('my commit description')
-# origin = repo.remote('origin')
-# origin.push()
-'''
-
-'''
-print("showing the differenced files")
-for item in repo.index.diff("HEAD~28"):
-    print(item.a_path)
-
-print("showing the differenced files1")
-for item in repo.index.diff(None):
-    print(item.a_path)
-
-print("showing the differenced files2")
-for item in repo.index.diff("HEAD"):
-    print(item.a_path)
-diff = repo.git.diff('HEAD~1..HEAD', name_only=True)
-print(diff)
-'''
-
-'''
-print("showing the differenced file0")
-print(repo.index.diff(other="HEAD~18"))
-for item in repo.index.diff(other="HEAD~18"):
-    print(item.a_path)
-
-print("showing the differenced file1")
-print(repo.index.diff(other="HEAD~18",paths="HEAD~17"))
-for item in repo.index.diff("HEAD~18","HEAD~17"):
-    print(item.a_path)
-'''
-
-'''
-
-commits_list = list(repo.iter_commits())
-print("length",len(commits_list))
-# print("latest commit id: ", commits_list[0])
-# print("first commit id: ", commits_list[-1])
-
-
-# print("accessing the data")
-# head = commits_list[0]
-# print(head)
-# print(head.parents)
-# print(head.author)
-# print(head.message)
-# print(type(commits_list[0]))
-print("accessing the tree")
-tree = commits_list[0].tree
-# print(tree)
+#############Another method ############
 if 0:
-    print("printing the file names")
-    print("blobs",tree.blobs)
-    print("blob1",tree.blobs[0])
-    for b,i in enumerate(tree.blobs):
-        print(b,i.name)
-if 0:
-    print("printing the folder names")
-    print("trees",tree.trees)
-    print("trees1",tree.trees[0])
-    for b,i in enumerate(tree.trees[0].trees[0].blobs):
-        print(b,i.name)
-'''
+    # we can use the below logic also to push the code
+    repo.git.add('--all')
+    repo.index.add(['bla.txt'])#for sing file addition
+    repo.index.commit('my commit description')
+    origin = repo.remote('origin')
+    origin.push()
 
-if 0:
-    print("getting all commits")
-    commits_list = list(repo.iter_commits())
-    print("length",len(commits_list))
-    print("latest commit id: ", commits_list[0])
-    print("first commit id: ", commits_list[-1])
-
-    changed_files = []
-    for x in commits_list[0].diff(commits_list[25]):
-        if x.a_blob.path not in changed_files:
-            changed_files.append(x.a_blob.path)
-
-        if x.b_blob is not None and x.b_blob.path not in changed_files:
-            changed_files.append(x.b_blob.path)
-
-    print("printing the files changed between specified commits")
-    for i in changed_files:
-        print(i)
-
-
-if 0:
+if 0:#printing the commits from specific branch
     print("printing the commits from branch")
     for i in list(repo.iter_commits('master', max_count=10)):
         print(i)
+
+
+if 0:
+    commits_list = list(repo.iter_commits())
+    print("total commits", len(commits_list))
+    print("latest commit id: ", commits_list[0])
+    print("first commit id: ", commits_list[-1])
+    head = commits_list[0]
+    print(head)
+    print(head.author)
+    print("accessing the tree")
+    tree = commits_list[0].tree
+
+    if 0:
+        print("printing the file names")
+        print("blobs",tree.blobs)
+        print("blob1",tree.blobs[0])
+        for b,i in enumerate(tree.blobs):
+            print(b,i.name)
+    if 0:
+        print("printing the folder names")
+        print("trees",tree.trees)
+        print("trees1",tree.trees[0])
+        for b,i in enumerate(tree.trees[0].trees[0].blobs):
+            print(b,i.name)
+
+if 0:#printing the files changed between specified commits
+    if 0:#first logic
+        print("getting all commits")
+        commits_list = list(repo.iter_commits())
+
+        changed_files = []
+        for x in commits_list[0].diff(commits_list[25]):
+            if x.a_blob.path not in changed_files:
+                changed_files.append(x.a_blob.path)
+
+            if x.b_blob is not None and x.b_blob.path not in changed_files:
+                changed_files.append(x.b_blob.path)
+
+        print("printing the files changed between specified commits")
+        for i in changed_files:
+
+            print(i)
+
+    if 0:#Another method
+        print("printing the files changed between specified commits1")
+        diff = repo.git.diff('HEAD~25..HEAD', name_only=True)
+        print(diff)
+
+    if 0:#Other logic
+        print("showing the differenced files")
+        for item in repo.index.diff("HEAD~25"):
+            print(item.a_path)
+
 
 '''
 # to display the file changes between pulls
