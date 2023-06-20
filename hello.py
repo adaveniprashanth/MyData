ac20d4caf683583bf13632e2556400a3dc86b632
adaveniprashanth <adaveniprashanth@gmail.com>
2023-06-20 18:43:03 +0530

    Update hello.py
    added new changes

--- hello.py
+++ hello.py
@@ -4,11 +4,11 @@ print("new changes")
 print("hello world")
 print("new from local again and again")
 # checking subprocess and os module for installing and running the commands and any other operations
-subprocess.run("pip install selenium")
-os.system("pip uninstall selenium")
+"subprocess.run("pip install selenium")
+"os.system("pip uninstall selenium")
 print("hello world")
 
-
+"""
 project name--> AXG_MediaIP_Val_PSVCOE
 
 Employee Id	CSB03603
@@ -20,3 +20,4 @@ New Po Number	ceri-intc-425
 Old Customer Po Number	ceri-int-002
 New Customer Po Number	3001834106
 Effective Date	12-12-2022
+"""
